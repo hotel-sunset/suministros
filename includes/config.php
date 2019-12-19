54c82475927c5c1ec25c7d84a9ad9b16bafeb4d7
ing-alfredo <dj_97@outlook.com>
2019-12-18 18:04:21 -0600

    cambio de config

--- includes/config.php
+++ includes/config.php
@@ -3,8 +3,8 @@
 
 */
   define( 'DB_HOST', 'localhost' );          // Set database host
-  define( 'DB_USER', 'root' );             // Set database user
-  define( 'DB_PASS', '' );             // Set database password
-  define( 'DB_NAME', 'hotel_sunset' );        // Set database name
+  define( 'DB_USER', 'id11988297_royal' );             // Set database user
+  define( 'DB_PASS', 'sunsetroyal' );             // Set database password
+  define( 'DB_NAME', 'id11988297_hotel_sunset' );        // Set database name
 
 ?>
